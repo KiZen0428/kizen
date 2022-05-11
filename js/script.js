8b178ff5c7298a26d7dc55735a3185afe97eb0a3
Tina Yue <tinayue@Tinas-MacBook-Air-2.local>
2022-05-11 15:38:30 +0900

    changed

--- js/script.js
+++ js/script.js
@@ -78,3 +78,54 @@ $('#slider').vegas({
   animation: 'random',
   slides: responsiveImage,
 });
+
+
+
+
+
+
+
+
+
+
+// new hidding navi start
+
+var beforePos = 0;
+
+function ScrollAnime() {
+  var elemTop = $("#line-up").offset().top; 
+  var scroll = $(window).scrollTop();
+  
+  if (scroll == beforePos) {
+   
+  } else if (elemTop > scroll || 0 > scroll - beforePos) {
+    
+    $("#header").removeClass("UpMove"); 
+    $("#header").addClass("DownMove"); 
+  } else {
+    
+    $("#header").removeClass("DownMove"); 
+    $("#header").addClass("UpMove");
+  }
+
+  beforePos = scroll; 
+}
+
+
+$(window).scroll(function () {
+  ScrollAnime();
+});
+
+
+$(window).on("load", function () {
+  ScrollAnime();
+});
+
+
+var headerH = $("#header").outerHeight(true);
+$("#nav-list li a").click(function () {
+  var elmHash = $(this).attr("href");
+  var pos = $(elmHash).offset().top - headerH;
+  $("body,html").animate({ scrollTop: pos }, 1000);
+  return false;
+});
